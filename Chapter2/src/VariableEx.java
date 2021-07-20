7a3610f96537f56e5daee14a27f080eca96765c2
jeonglim <wjdfla401@naver.com>
2021-07-20 16:31:39 +0900

    Variable Example

---
+++ Chapter2/src/VariableEx.java
@@ -0,0 +1,22 @@
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+		
+		int num; // 변수를 선언
+		num = 10; // 값을 대입하는 개념이다.
+		
+		System.out.println(num);
+		
+		int level = 20; // 선언과 동시에 초기화 가능
+		System.out.println(level);
+		
+		// 변수 이름 사이에 공백이 있으면 안된다.
+		// 변수의 이름은 숫자로 시작할 수 없고, 키워드도 변수의 이름으로 사용할 수 없다.
+		// 변수의 이름에 의미를 주는 것이 좋다.
+		
+		int ns;
+		int numberOfStudent; // 학생의 수에 대해서 변수의 이름을 정할 때 아래쪽이 가독성이 좋다.
+	}
+
+}
