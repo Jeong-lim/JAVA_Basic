7b231cbb8f9d465be8c90006c9ac57aa7587e827
jeonglim <wjdfla401@naver.com>
2021-07-21 23:06:04 +0900

    Operation Example

--- Chapter3/src/OperationEx4.java
+++ Chapter3/src/OperationEx4.java
@@ -9,6 +9,9 @@ public class OperationEx4 {
 		System.out.println(num1 %= 10); 
 		num1 -= 1; //  num1 = num1 - 1;
 		System.out.println(num1);
+		
+		int num = (5 > 3)? 10:20;
+		System.out.println(num);
 
 	}
 
