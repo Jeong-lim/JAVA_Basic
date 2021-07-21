d7081eb85976b9d9016ce1e759a5403dc5f05363
jeonglim <wjdfla401@naver.com>
2021-07-21 22:59:33 +0900

    Operation Example

---
+++ Chapter3/src/OperationEx2.java
@@ -0,0 +1,19 @@
+
+public class OperationEx2 {
+
+	public static void main(String[] args) {
+		
+		int num = 10;
+		System.out.println(++num); // 11
+		// 이 문장이 끝나기 전에 증가시켜라
+		
+		System.out.println(num);
+		
+		System.out.println(num--); // 10
+		//이 문장이 끝나고 나면 감소시켜라
+		System.out.println(num); // 9
+		
+		
+	}
+
+}
