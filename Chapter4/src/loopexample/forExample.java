394e51f3b0ddbe2482211e9579d53c9ba5f3bc40
jeonglim <wjdfla401@naver.com>
2021-07-26 17:45:59 +0900

    for Example

---
+++ Chapter4/src/loopexample/forExample.java
@@ -0,0 +1,29 @@
+package loopexample;
+
+public class forExample {
+
+	public static void main(String[] args) {
+		
+		int dan = 2;
+		int times = 1;
+		
+		
+		for(dan = 2; dan <= 9; dan++) {
+			
+			for(times = 1; times <= 9; times++) {
+				System.out.println(dan + "X" + times + "=" + dan*times);
+			}
+			System.out.println(); // °³Çà
+		}
+		
+		while(dan <=9) {
+			while(times <= 9) {
+				System.out.println(dan + "X" + times + "=" + dan*times);
+				times++;
+			}
+			dan++;
+			System.out.println();
+		}
+	}
+
+}
