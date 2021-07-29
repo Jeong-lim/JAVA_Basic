a99006f97afdf618ea498d11c8d50727144f1f71
jeonglim <wjdfla401@naver.com>
2021-07-29 22:23:20 +0900

    Chapter4

---
+++ Chapter4/src/loopExample/forExample2.java
@@ -0,0 +1,18 @@
+package loopExample;
+
+public class forExample2 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println("hello, world");
+		}
+		
+		System.out.println();
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println("hello, world");
+		}
+	}
+
+}
