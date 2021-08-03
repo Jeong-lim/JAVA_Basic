03252b0ec2eacac1025570ad728e8a1e20720945
jeonglim <wjdfla401@naver.com>
2021-08-03 16:54:59 +0900

    Boolean 예제

--- BooleanDataType/src/BooleanApp.java
+++ BooleanDataType/src/BooleanApp.java
@@ -6,6 +6,15 @@ public class BooleanApp {
 		System.out.println("One");
 		System.out.println(1);
 
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello world";
+		// String true = "Hello world"  reserved word
+		// 따음표가 없는 true는 변수의 이름으로 사용 불가 
+		
+		System.out.println(foo.contains("world"));
+		
 	}
 
 }
