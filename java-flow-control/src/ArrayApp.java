b74d6fb0027b09c1b20ac5fc191e913940f1c64b
Jeong-lim <wjdfla401@naver.com>
2021-08-05 17:25:59 +0900

    Array Example

---
+++ java-flow-control/src/ArrayApp.java
@@ -0,0 +1,23 @@
+
+public class ArrayApp {
+
+	public static void main(String[] args) {
+		
+		// egoing, jeonglim, junheok
+		
+//		String users = "egoing, jeonglim, junheok"
+		String[] users = new String[3];
+		users[0] = "jeonglim";
+		users[1] = "egoing";
+		users[2] = "junheok";
+		
+		System.out.println(users[0]);
+		System.out.println(users.length);
+		
+		int[] scores = {10, 100, 100};
+		System.out.println(scores[1]);
+		System.out.println(scores.length);
+		
+	}
+
+}
