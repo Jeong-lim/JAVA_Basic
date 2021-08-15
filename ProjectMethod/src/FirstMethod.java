b45c32916067372ddd13719283a0cd678105fd44
Jeong-lim <wjdfla401@naver.com>
2021-08-15 22:55:19 +0900

    method 예제

--- ProjectMethod/src/FirstMethod.java
+++ ProjectMethod/src/FirstMethod.java
@@ -1,7 +1,7 @@
 
 public class FirstMethod {
 
-	public static void main(String[] args) { // main이라는 이름의 method
+	public static void main(String[] args) { // main이라는 이름의 method다.
 		
 		System.out.println("Hello Method");
 		System.out.println(Math.floor(1.1));
