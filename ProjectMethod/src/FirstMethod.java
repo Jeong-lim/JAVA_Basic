985181445deda9b99218df683f1c7df5daf04aaf
Jeong-lim <wjdfla401@naver.com>
2021-08-16 23:42:54 +0900

    first method 예제

--- ProjectMethod/src/FirstMethod.java
+++ ProjectMethod/src/FirstMethod.java
@@ -4,7 +4,10 @@ public class FirstMethod {
 	public static void main(String[] args) { // main이라는 이름의 method다.
 		
 		System.out.println("Hello Method");
-		System.out.println(Math.floor(1.1));
+		System.out.println(Math.floor(1.1)); // floor 그리고 인자로 1.1을 주게되면 저 메소드는 1.0이 된다.
+		
+		
+		
 	}
 
 }
