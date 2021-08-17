ed0281cc70f84330e0931468c5bbdbdb8ef57cb4
Jeong-lim <wjdfla401@naver.com>
2021-08-18 01:22:08 +0900

    객체지향 예제

--- ProjectOOP/src/Print.java
+++ ProjectOOP/src/Print.java
@@ -1,16 +1,20 @@
-
 class Print {
-	public static String delimiter  = "";
-	// static 이라는 것은 String은 Class의 소속이라는 것
-	// 즉 static은 class의 소속 
-	public static void A() {
-		System.out.println(delimiter);
+	public String delimiter  = "";
+	public Print(String delimiter) {
+		this.delimiter = delimiter;
+	}
+	public void A() {
+		System.out.println(this.delimiter);
 		System.out.println("A");
 		System.out.println("A");
 	}
-	public static void B() {
-		System.out.println(delimiter);
+	public void B() {
+		System.out.println(this.delimiter);
 		System.out.println("B");
 		System.out.println("B");
 	}
+// static 이라는 것은 String은 Class의 소속이라는 것
+// 즉 static은 class의 소속 
+// 생성자는 초기에 주입할 필요가 있는 값을 전달하거나, 또는 초기에 작업을 수행할 때 쓴다.
+// this는 그 클래스가 인스턴스화 되었을 때에 인스턴스를 가르키는 특수한 이름이다.	
 }
